9c1a0739b211d915e682ecc6f452ba436c72d78e
skwon8 <saengsukwon@gmail.com>
2022-03-15 15:32:05 -0400

    Using practice Hook Form to do More Forms.

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -24,14 +24,24 @@ const HookForm = () => {
                 <div className="form-group">
                     <label htmlFor="">First Name</label>
                     <input type="text" name="" id="" className="form-control" onChange = {(e) => setfirstName(e.target.value)} />
+                    {firstname.length < 2 ? "First name must be at least 2 characters": ""}
+
                     <label htmlFor="">Last Name</label>
                     <input type="text" name="" id="" className="form-control" onChange = {(e) => setlastName(e.target.value)} />
+                    {lastname.length < 2 ? "Last name must be at least 2 characters": ""}
+
                     <label htmlFor="">Email</label>
                     <input type="email" name="" id="" className="form-control" onChange = {(e) => setEmail(e.target.value)} />
+                    {email.length < 5 ? "Email must be at least 5 characters": ""}
+
                     <label htmlFor="">Password</label>
                     <input type="password" name="" id="" className="form-control" onChange = {(e) => setPassword(e.target.value)} />
+                    {password.length < 8 ? "Password must be at least 8 characters": ""}
+
                     <label htmlFor="">Confirm Password</label>
                     <input type="password" name="" id="" className="form-control" onChange = {(e) => setConfirmPassword(e.target.value)}/>
+                    {confirmpassword.length < 8 ? "Confirm Password must be at least 8 characters": ""}
+
                 </div>
             </form>
             <p>First Name: {firstname}</p>
